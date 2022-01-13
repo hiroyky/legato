14f8faaa09781a649cf512650f2ed848a11936d3
Hirokazu Yokoyama <hiroyky.voyager@gmail.com>
2022-01-14 03:50:25 +0900

    不要箇所削除

--- graph/track.resolvers.go
+++ graph/track.resolvers.go
@@ -5,8 +5,6 @@ package graph
 
 import (
 	"context"
-	"fmt"
-
 	"github.com/hiroyky/legato/graph/generated"
 	"github.com/hiroyky/legato/graph/gqlmodel"
 	"github.com/hiroyky/legato/lib/gql"
@@ -53,13 +51,3 @@ func (r *trackResolver) AlbumArtist(ctx context.Context, obj *gqlmodel.Track) (*
 func (r *Resolver) Track() generated.TrackResolver { return &trackResolver{r} }
 
 type trackResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *trackResolver) DownURL(ctx context.Context, obj *gqlmodel.Track) (string, error) {
-	panic(fmt.Errorf("not implemented"))
-}
